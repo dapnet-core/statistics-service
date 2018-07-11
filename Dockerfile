2c116781cf01c9a68edf165512c9c6de3b5b2ecd
root <root@dapnetdc1.db0sda.ampr.org>
2018-07-11 20:55:26 +0200

    Added REST call to CouchDB for users count

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3
 
 ADD statistics-service.py  /
 
-RUN pip3 install flask && pip3 install flask-restful
+RUN pip3 install flask && pip3 install flask-restful && pip3 install requests
 
 # private and public mapping
 EXPOSE 80
--- statistics-service.py
+++ statistics-service.py
@@ -1,14 +1,38 @@
 from flask import Flask, jsonify
-#from flask_restful import Resource, Api
 import platform
+import requests
+
+# Edit in final deploy
+CouchDB_Stat_users_URL = 'http://dapnetdc1.db0sda.ampr.org:5984/users'
+CouchDB_Stat_transmitters_URL = 'http://dapnetdc1.db0sda.ampr.org:5984/transmitters'
+
 
 app = Flask(__name__)
+
+@app.route('/statistics/info')
+def api_statistics_info():
+    data = {}
+    data['version'] = '0.0.3'
+    data['python_version'] = platform.python_version()
+    data['microservice'] = 'statistics'
+    return jsonify(data)
+
 
 @app.route('/statistics')
 def api_statistics():
     data = {}
-    data['microservice'] = 'statistics'
-    data['python_version'] = platform.python_version()
+
+# Get total users
+    response = requests.get(url=CouchDB_Stat_users_URL)
+    if response.status_code == 200:
+        response_data = response.json()
+        data['users'] = response_data['doc_count']
+    else:
+        data['users'] = -1
+
+#Get total transmitters
+#This needs a view, as the transmitters have to be counted depending on their type
+
     return jsonify(data)
 
 if __name__ == '__main__':
