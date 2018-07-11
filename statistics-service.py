0fdbdbfecbde86fb617882e607f22b971ea1658a
root <root@dapnetdc1.db0sda.ampr.org>
2018-07-11 12:25:51 +0200

    Added resource handling

--- statistics-service.py
+++ statistics-service.py
@@ -1,18 +1,15 @@
 from flask import Flask, jsonify
-from flask_restful import Resource, Api
+#from flask_restful import Resource, Api
 import platform
 
 app = Flask(__name__)
-api = Api(app)
 
-class Status(Resource):
-    def get(self):
-        data = {}
-        data['microservice'] = 'statistics'
-        data['python_version'] = platform.python_version()
-        return (data)
-
-api.add_resource(Status, '/statistics')
+@app.route('/statistics')
+def api_statistics():
+    data = {}
+    data['microservice'] = 'statistics'
+    data['python_version'] = platform.python_version()
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
