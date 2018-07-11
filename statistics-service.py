1d935d95723169b1470ca441b0cc49514e325383
root <root@dapnetdc1.db0sda.ampr.org>
2018-07-11 22:35:52 +0200

    Added working query of CouchDB for transmitter total values

--- statistics-service.py
+++ statistics-service.py
@@ -4,7 +4,7 @@ import requests
 
 # Edit in final deploy
 CouchDB_Stat_users_URL = 'http://dapnetdc1.db0sda.ampr.org:5984/users'
-CouchDB_Stat_transmitters_URL = 'http://dapnetdc1.db0sda.ampr.org:5984/transmitters'
+CouchDB_Stat_transmitters_URL = 'http://dapnetdc1.db0sda.ampr.org:5984/transmitters/_design/statistics/_view/count?group_level=1'
 
 
 app = Flask(__name__)
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/statistics/info')
 def api_statistics_info():
     data = {}
-    data['version'] = '0.0.4'
+    data['version'] = '0.0.5'
     data['python_version'] = platform.python_version()
     data['microservice'] = 'statistics'
     return jsonify(data)
@@ -32,27 +32,19 @@ def api_statistics():
 
 #Get total transmitters
     data['transmitters'] = {}
-#Widerange
     data['transmitters']['widerange'] = {}
-    response = requests.get(url=CouchDB_Stat_transmitters_URL + '_design/docs/_view/countwiderangetotal')
-    if response.status_code == 200:
-        response_data = response.json()
-#No idea how the output of the view looks like
-        data['transmitters']['widerange']['total'] = 11
-    else:
-        data['transmitters']['widerange']['total'] = -1
-
-#Get total widerange transmitters
     data['transmitters']['personal'] = {}
-    response = requests.get(url=CouchDB_Stat_transmitters_URL + '_design/docs/_view/countpersonaltotal')
+
+    response = requests.get(url=CouchDB_Stat_transmitters_URL)
     if response.status_code == 200:
         response_data = response.json()
-#No idea how the output of the view looks like
-        data['transmitters']['personal']['total'] = 11
-    else:
-        data['transmitters']['personal']['total'] = -1
-    return jsonify(data)
+        for row in response_data['rows']:
+            if row['key'] == 'PERSONAL':
+                data['transmitters']['personal']['total'] = row['value']
+            elif row['key'] == 'WIDERANGE':
+                data['transmitters']['widerange']['total'] = row['value']
 
+    return jsonify(data)
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
 
